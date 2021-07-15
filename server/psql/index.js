c9b79f2dd4de595d4a2b680d3b2bfd59b92dd8e2
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-07-14 20:50:37 -0700

    Setup PostgreSQL database connection with 'pg' module

--- server/psql/index.js
+++ server/psql/index.js
@@ -1,7 +1,8 @@
 const { Client } = require('pg');
 
 const client = new Client();
+
 client
   .connect()
-  .then(() => console.log('connected'))
-  .catch((err) => console.log('connection error', err.stack));
+  .then(() => console.log('PostgreSQL database connection established!'))
+  .catch((err) => console.log('PostgreSQL database connection error', err.stack));
