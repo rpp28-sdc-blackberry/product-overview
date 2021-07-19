e18f068a7481f5ca8fe69a737da01dc0c2437938
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-07-19 01:25:22 -0700

    Initialize API server, define routes, and setup frisby tests

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 8080;
-
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+app.get('/products', (req, res) => {
+  // TODO
+  res.send();
+});
+
+app.get('/products/:product_id', (req, res) => {
+  let productId = req.params.product_id;
+  // TODO
+  res.send();
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  let productId = req.params.product_id;
+  // TODO
+  res.send();
+});
+
+app.get('/products/:product_id/related', (req, res) => {
+  let productId = req.params.product_id;
+  // TODO
+  res.send();
+});
+
+app.listen(process.env.SVPORT, () => {
+  console.log(`Listening at http://localhost:${process.env.SVPORT}`);
 });
