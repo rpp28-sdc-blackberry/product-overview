1777c3ae12ec4c416311755d48a1552badb02805
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-07-20 21:01:37 -0700

    Add additional tests for products/:product_id endpoint

--- test/routing.test.js
+++ test/routing.test.js
@@ -10,11 +10,54 @@ xdescribe('/products endpoint', () => {
   });
 });
 
-xdescribe('/products/:product_id endpoint', () => {
-  it('should return a status of 200', function () {
+describe('/products/:product_id endpoint', () => {
+  it('should return a status of 200', () => {
     return frisby
       .get(`${baseURL}/products/1`)
       .expect('status', 200);
+  });
+  it('should return the correct product id', () => {
+    return frisby
+      .get(`${baseURL}/products/1`)
+      .then(res => {
+        expect(res.json.id).toBe(1);
+      });
+  });
+  it('should return the correct product name', () => {
+    return frisby
+      .get(`${baseURL}/products/1`)
+      .then(res => {
+        expect(res.json.name).toBe('Camo Onesie');
+      });
+  });
+  it('should return the correct product slogan', () => {
+    return frisby
+      .get(`${baseURL}/products/1`)
+      .then(res => {
+        expect(res.json.slogan).toBe('Blend in to your crowd');
+      });
+  });
+  it('should return the correct product description', () => {
+    let description = 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.';
+    return frisby
+      .get(`${baseURL}/products/1`)
+      .then(res => {
+        expect(res.json.description).toBe(description);
+      });
+  });
+  it('should return the correct product category', () => {
+    return frisby
+      .get(`${baseURL}/products/1`)
+      .then(res => {
+        expect(res.json.category).toBe('Jackets');
+      });
+  });
+  it('should return the correct product default price', () => {
+    return frisby
+      .get(`${baseURL}/products/1`)
+      .then(res => {
+        expect(res.json.default_price).toBe('140');
+      });
   });
 });
 
