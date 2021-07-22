555128f420dbf26cd879f8296ed79c5baee59fff
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-07-21 20:30:15 -0700

    Add additional tests for /related endpoint/route

--- test/routes/related.test.js
+++ test/routes/related.test.js
@@ -3,10 +3,24 @@ const frisby = require('frisby');
 const baseURL = `http://localhost:${process.env.SVPORT}`;
 
 describe('/products/:product_id/related endpoint', () => {
-  it('should return a status of 200', function () {
+  it('should return a status of 200 for a valid product id', function () {
     return frisby
       .timeout(30000)
       .get(`${baseURL}/products/3/related`)
       .expect('status', 200);
+  });
+  it('should return a status of 500 for an invalid product id', function () {
+    return frisby
+      .timeout(30000)
+      .get(`${baseURL}/products/-1/related`)
+      .expect('status', 500);
+  });
+  it('should return the correct related product ids', () => {
+    return frisby
+      .timeout(30000)
+      .get(`${baseURL}/products/1/related`)
+      .then(res => {
+        expect(res.json).toEqual([2, 3, 8, 7]);
+      });
   });
 });
