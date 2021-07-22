02fbe446e70c2bda561ac171feb5859ca7e801e6
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2021-07-21 19:33:31 -0700

    Add additional tests for /products endpoint/route

--- test/routes/products.test.js
+++ test/routes/products.test.js
@@ -9,4 +9,45 @@ describe('/products endpoint', () => {
       .get(`${baseURL}/products`)
       .expect('status', 200);
   });
+  it('should start at product id 1 by default', () => {
+    return frisby
+      .timeout(30000)
+      .get(`${baseURL}/products`)
+      .then(res => {
+        expect(res.json[0].id).toBe(1);
+      });
+  });
+  it('should return 5 products by default', () => {
+    return frisby
+      .timeout(30000)
+      .get(`${baseURL}/products`)
+      .then(res => {
+        expect(res.json.length).toBe(5);
+      });
+  });
+  it('should start at at appropriate product id when page is specified', () => {
+    return frisby
+      .timeout(30000)
+      .get(`${baseURL}/products?page=2`)
+      .then(res => {
+        expect(res.json[0].id).toBe(6);
+      });
+  });
+  it('should return an appropriate number of products when count is specified', () => {
+    return frisby
+      .timeout(30000)
+      .get(`${baseURL}/products?count=10`)
+      .then(res => {
+        expect(res.json.length).toBe(10);
+      });
+  });
+  it('should handle page and count parameters concurrently', () => {
+    return frisby
+      .timeout(30000)
+      .get(`${baseURL}/products?page=3&count=15`)
+      .then(res => {
+        expect(res.json[0].id).toBe(31);
+        expect(res.json.length).toBe(15);
+      });
+  });
 });
